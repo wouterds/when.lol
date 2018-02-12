af2901183914e62e1d1db24e21278b7a9def625e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-02-12 14:52:03 +0100

    :sparkles: Added GalleryItemId

---
+++ src/Domain/Gallery/GalleryItemId.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WouterDeSchuyter\WhenLol\Domain\Gallery;
+
+use WouterDeSchuyter\WhenLol\Infrastructure\ValueObjects\AbstractUuid;
+
+class GalleryItemId extends AbstractUuid
+{
+}
